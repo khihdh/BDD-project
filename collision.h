42c9b559e3409dafd0ab3dd6237398c42f375072
KERENCILER <krnclr.ismet@gmail.com>
2022-05-30 17:42:07 +0000

    Update collision.h

--- collision.h
+++ collision.h
@@ -3,7 +3,8 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 
-// Classe dediee pour la gestion des collisions
+//cette classe permet de tirer aléatoirement dans l’espace les coordonnées des astéroïdes ainsi que leur diamètre et de vérifier 
+//qu’il n’y a pas de collision entre chaque astéroïde en calculant la norme entre deux astéroïdes
 
 class collision
 {
@@ -18,8 +19,12 @@ private:
     GLdouble tabd_[nbAstMax];
 public:
     collision(int nbAst);
+    //cette fonction permet de set le nombre d'asteroides choisit par le joueur
     void setNbAst(int nbAst2);
     float RandomFloat(float min, float max);
+    //cette fonction permet de choisir aleatoirement les coordonnees des asteroides dans l'espace ainsi que leur diametre et de 
+    //checker si les asteroides n'entrent pas en colission les uns avec les autres
+    //param: les tableaux de flottant qui doivent être remplis par les coordonnees des asteroides et le tableau des diametres des asteroides
     bool checkAstCol(float tabx_[nbAstMax], float taby_[nbAstMax], float tabz_[nbAstMax], GLdouble tabd_[nbAstMax]);
 };
 
