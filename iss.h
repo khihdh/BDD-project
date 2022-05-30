b21ae5ea0cd3bb8cd901bb9437351aab41f6adbe
KERENCILER <krnclr.ismet@gmail.com>
2022-05-30 18:12:01 +0000

    Update iss.h

--- iss.h
+++ iss.h
@@ -5,6 +5,9 @@
 #include <GL/glu.h>
 #include <QColor>
 
+//La classe ISS permet de dessiner la station orbitale, de lui associer une texture et la faire tourner sur elle-même. Elle gère également l’arrimage 
+//avec le vaisseau à l’aide d’une fonction basé sur le même principe de la détection de collision entre astéroïdes. La station tourne sur elle-même 
+//autour de l’axe y. 
 class ISS
 {
 private:
@@ -15,6 +18,9 @@ public:
     ISS();
     ~ISS();
     void Display(uint64_t iTimeElapsed);
+    //cette fonction permet de checker si le vaisseau s'est arrimé à la station orbital en utilisant le principe de detection de colission
+    //param : coordonnées du vaisseau
+    //return : 1 si il y a arrimage et -1 sinon
     int CheckAshing(float x, float y, float z);
 };
 
