26b58bb89d472b1e35ce647c3761b4982a6afd36
KERENCILER <krnclr.ismet@gmail.com>
2022-05-30 18:08:46 +0000

    Update spaceship.h

--- spaceship.h
+++ spaceship.h
@@ -6,7 +6,7 @@
 #include <GL/glu.h>
 #include <QColor>
 
-// Classe dediee pour la gestion de la voiture
+// Cette classe permet de dessiner le vaisseau et de representer le vaisseau dans un repere spherique
 class SpaceShip
 {
 public:
@@ -17,13 +17,17 @@ public:
     ~SpaceShip();
 
     // Methode d'affichage
+    //cette fonction permet de dessiner le vaisseau et de lui appliquer des rotations et des translation dans le repère sphérique
     void Display(uint64_t iTimeElapsed) const;
     void incrCoordinatesZSpaceship(float teta2,float phi2,float r);
     void incrCoordinatesYSpaceship();
     void incrCoordinatesXSpaceship();
+    //on exprime les coordonnes cartesiennes du vaisseau en fonction de ses coordonees spherique
     void rotPhi(float phi,float teta,float r);
+    //ces fonctions permettent de changer le flag de l'angle theta ou phi
     void changeFlagTeta(bool flag);
     void changeFlagPhi(bool flag);
+    //cette fonction permet de reinitialiser les coordonnees spheriques du vaisseau
     void reset();
 
 private:
