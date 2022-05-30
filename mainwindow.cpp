04a142bb9311fc801d5e001f742a38d5460a662a
KERENCILER <krnclr.ismet@gmail.com>
2022-05-30 16:40:48 +0000

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,9 +9,10 @@
 #include <QFileDialog>
 #include "mainwindow.h"
 
+// Cette classe va gérer l'interface utilisateur principale avec le joueur
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-{
+{   
     label =new QLabel;
     label->setText("Interstellar'tse Game !");
     label->setVisible(true);
@@ -42,8 +43,6 @@ MainWindow::MainWindow(QWidget *parent)
     spinBox->setAlignment(Qt::AlignCenter);
 
     QVBoxLayout *layout = new QVBoxLayout;
-    //layout->addWidget(startB = new QPushButton("start", this));
-    //layout->addWidget(stopB = new QPushButton("stop", this));
     layout->addWidget(label);
     layout->addWidget(nbAst_Label);
     layout->addWidget(spinBox);
@@ -53,9 +52,8 @@ MainWindow::MainWindow(QWidget *parent)
     layout->addWidget(btnPlay);
     layout->addWidget(btnQuit);
 
+    //On connecte le timer t à la fonction updateTime() pour actualiser le chronomètre toute les secondes
     connect(&t, SIGNAL(timeout()), this, SLOT(updateTime()));
-    //connect(btnPlay, SIGNAL(clicked(bool)), this, SLOT(start()));
-    //connect(stopB, SIGNAL(clicked(bool)), &t, SLOT(stop()));
 
     QWidget *centralWidget = new QWidget;
     centralWidget->setLayout(layout);
@@ -63,6 +61,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     timerMessageBox = new QTimer(this);
 
+    //On connecte le timer à la fonction checkGameOver() pour qu'elle soit appeler toute les secondes 
     QObject::connect(timerMessageBox, SIGNAL(timeout()), this, SLOT(checkGameOver()));
 
 
@@ -74,12 +73,18 @@ MainWindow::~MainWindow()
 
 }
 
+//Cette fonction permet de initialiser une partie et offre la possibilité au joueur de rejouer
 void  MainWindow::play(){
+    //gameOverFlag = false => la partie est en cours
     gameOverFlag=false;
+    //On (ré)initialise le chronomètre à 0. 
     l->setText(QTime(0, 0).toString());
+    //On restart le timer du chronomètre après l'avoir stop à la fin de la partie précédente si le joueur rejoue
     startTime.restart();
     t.start(1000);
+    //On restart le timer qui permet d'appeler la fonction checkGameOverFlag() 
     timerMessageBox->start(1000);
+    //On récupère la difficulté choisis par l'utilisateur
     int diff = spinBox->value();
     nbAst = 0;
     winLoose_str = "The game is live !";
@@ -93,16 +98,20 @@ void  MainWindow::play(){
     else if (diff == 3) {
         detectFist_.setNbAst(16);
     }
+    //On lance la webcam et la scène
     detectFist_.show();
 }
 int MainWindow::getNbAst() {
     return nbAst;
 }
+
+// Cette fonction permet de checker toute les secondes si la partie est terminé ou pas afin d'en informer le joueur
 void MainWindow::checkGameOver()
 {
     if (!gameOverFlag) {
         if  (detectFist_.checkGameOverFlag()) {
             t.stop();
+            //on reset les coordonnées du vaisseau
             detectFist_.reset();
             winLoose_str = "You loose ! Your score is just below. Click to play to restart the game.";
             winLoose_Label->setText(winLoose_str);
@@ -126,10 +135,12 @@ void MainWindow::quit() {
     close();
 }
 
+//Cette fonction permet de mettre à jours l'affichage du chronomètre
 void MainWindow::updateTime() {
     l->setText(QTime(0, 0).addMSecs(startTime.elapsed()).toString());
 }
 
+//Cette fonction permet de lancer le chronomètre
 void MainWindow::start() {
       l->setText(QTime(0, 0).toString());
       startTime.restart();
